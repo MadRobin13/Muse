dc1a679ca04bba926e0bc8a81aae0220be15e354
Abhi <madrobin1340@gmail.com>
2025-06-01 15:31:56 -0400

    added some basic gyro code which works with the MPU-6050. Now I need to add some mouse control. This took WAY too long to debug.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
 
-// put function declarations here:
-int myFunction(int, int);
+MPU6050 mpu;
+
+float accAngleX, accAngleY;
+float gyroAngleX, gyroAngleY, gyroAngleZ;
+float roll, pitch, yaw;
+float elapsedTime, currentTime, previousTime;
+int16_t gyroX, gyroY, gyroZ;
+int16_t accX, accY, accZ;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(115200);
+
+  Wire.begin();
+
+  mpu.initialize();
+
+  previousTime = millis();
+
 }
 
 void loop() {
-  // digitalWrite()
-}
+  currentTime = millis();
+  elapsedTime = (currentTime - previousTime) / 1000.0;
+  previousTime = currentTime;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  mpu.getMotion6(&accX, &accY, &accZ, &gyroX, &gyroY, &gyroZ);
+
+  accAngleX = atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI;
+  accAngleY = atan(-accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI;
+
+  gyroAngleX += gyroX / 131.0 * elapsedTime;
+  gyroAngleY += gyroY / 131.0 * elapsedTime;
+  gyroAngleZ += gyroZ / 131.0 * elapsedTime;
+
+  roll = 0.96 * (roll + gyroX / 131.0 * elapsedTime) + 0.04 * accAngleX;
+  pitch = 0.96 * (pitch + gyroY / 131.0 * elapsedTime) + 0.04 * accAngleY;
+  yaw += gyroZ / 131.0 * elapsedTime;
+
+  Serial.print(" Roll: "); Serial.print(roll);
+  Serial.print(" Pitch: "); Serial.print(pitch);
+  Serial.print(" Yaw: "); Serial.println(yaw);
 }
